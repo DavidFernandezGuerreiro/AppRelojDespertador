264d4a7531e1fcb2e09e2bfdd25bf65b44b02dd6
David <David@LAPTOP-VSEKM5I2>
2018-03-13 16:16:11 +0000

    Init/finaly principal.

---
+++ src/apprelojdespertador/APPRelojDespertador.java
@@ -0,0 +1,18 @@
+
+package apprelojdespertador;
+
+/**
+ *
+ * @author David
+ */
+public class APPRelojDespertador {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Display obxD=new Display();
+        obxD.setVisible(true);
+    }
+    
+}
