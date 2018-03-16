b0643966bc0cbf0470ed06f0fa321572296d5c40
David <David@LAPTOP-VSEKM5I2>
2018-03-16 20:41:32 +0000

    Añadido javadoc.

--- src/apprelojdespertador/Despertador.java
+++ src/apprelojdespertador/Despertador.java
@@ -9,23 +9,34 @@ import javax.swing.Icon;
 import javax.swing.ImageIcon;
 import javax.swing.JOptionPane;
 
-
+/**
+ * Hilo, Timer, condición que ejecuta al Despertador.
+ * @author David
+ */
 public class Despertador {
+    /**
+     * 
+     */
     Timer timer;
     javax.swing.Timer tiempo;
     private int hor, min, seg;
     public static String hms;
     Alarma obxA=new Alarma();
     
+    /**
+     * Creamos un Timer.
+     * Con el Timer ejecutamos la tarea TicTac, con un retardo de 0sg y repetimos cada 1sg.
+     */
     public Despertador() {
-        //creamos un Timer
         timer = new Timer();
-        //con el Timer ejecutamos la tarea TicTac, con un retardo de 0sg y repetimos cada 1sg
         timer.schedule(new TicTac(), 0, 1000);
     }
     
     public class TicTac extends TimerTask {
 
+        /**
+         * El método run() es donde se realizará todo el trabajo de la clase.
+         */
         @Override
         public void run() {
             Date date=new Date();
@@ -36,6 +47,8 @@ public class Despertador {
             hms=hor+":"+min+":"+seg;
             Display.textoHora.setText(Despertador.hms);
             
+            //Si "hms" contiene la hora que introducimos en la alarma.
+            //Se ejecuta el despertador, mostrando un JOptionPane con un icono y texto.
             if(hms.contains(obxA.getHmss())){
                 Icon icon=new javax.swing.ImageIcon(getClass().getResource("/images/icono-despertador.png"));
                 JOptionPane.showMessageDialog(null, "*-_*-_*-_ RING _-*_-*_-*"
